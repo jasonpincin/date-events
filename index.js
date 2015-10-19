fbe5810493d41a76b54ed35e0311a2b7b20ca04e
Jason Pincin <jason@pincin.com>
2015-10-19 18:16:30 -0400

    formatting / refactor

--- index.js
+++ index.js
@@ -1,151 +1,69 @@
-var EventEmitter    = require('events').EventEmitter,
-    util            = require('util'),
-    DateEmitter;
+var EventEmitter = require('events').EventEmitter
 
-module.exports = DateEmitter = function (precision) {
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-    var self = this,
-        now = new Date(),
-        interval = 10000;
+module.exports = function createDateEmitter () {
 
-    EventEmitter.call(this);
+    var emitter  = new EventEmitter,
+        now      = new Date,
+        last     = {
+            year   : now.getFullYear(),
+            month  : now.getMonth() + 1,
+            date   : now.getDate(),
+            weekday: now.getDay(),
+            hour   : now.getHours(),
+            minute : now.getMinutes()
+        }
 
-    if (precision && precision <= 3600000) {
-        interval = precision;
-    }
+    return emitter
 
-    this.year = now.getFullYear();
-    this.month = now.getMonth() + 1;
-    this.date = now.getDate();
-    this.weekday = now.getDay();
-    this.hour = now.getHours();
-
-    var eventCollection = function() {
-        var str = '',
-            now = new Date(),
-
-            year = now.getFullYear(),
-            month = now.getMonth(),
-            date = now.getDate(),
+    function eachSecond () {
+        var now     = new Date,
+            year    = now.getFullYear(),
+            month   = now.getMonth() + 1,
+            date    = now.getDate(),
             weekday = now.getDay(),
-            hour = now.getHours();
+            hour    = now.getHours(),
+            minute  = now.getMinutes()
 
-        if (year !== self.year) {
-            self.emit('year', year);
-            self.year = year;
+        if (year !== last.year) {
+            emitter.emit('year', year)
+            last.year = year
         }
 
-        if (month + 1 !== self.month) {
-            str = '';
-            switch (month) {
-                case 0:
-                    str = 'January';
-                    break;
-                case 1:
-                    str = 'February';
-                    break;
-                case 2:
-                    str = 'March';
-                    break;
-                case 3:
-                    str = 'April';
-                    break;
-                case 4:
-                    str = 'May';
-                    break;
-                case 5:
-                    str = 'June';
-                    break;
-                case 6:
-                    str = 'July';
-                    break;
-                case 7:
-                    str = 'August';
-                    break;
-                case 8:
-                    str = 'September';
-                    break;
-                case 9:
-                    str = 'October';
-                    break;
-                case 10:
-                    str = 'November';
-                    break;
-                default:
-                    str = 'December';
-
-            }
-            self.emit('month', month + 1, str);
-            self.emit('month-' + str);
-            self.emit('month-' + str.substr(0, 3));
-            self.emit('month-' + (month + 1));
-            self.month = month + 1;
+        if (month !== last.month) {
+            emitter.emit('month', month, months[month - 1])
+            emitter.emit('month-' + months[month - 1])
+            emitter.emit('month-' + months[month - 1].substr(0, 3))
+            emitter.emit('month-' + month)
+            last.month = month
         }
 
-        if (date !== self.date) {
-            self.emit('date', date);
-            self.emit('date-' + date);
-            self.date = date;
+        if (date !== last.date) {
+            emitter.emit('date', date)
+            emitter.emit('date-' + date)
+            last.date = date
         }
 
-        if (weekday !== self.weekday) {
-            str = '';
-            switch (weekday) {
-                case 1:
-                    str = 'Monday';
-                    break;
-                case 2:
-                    str = 'Tuesday';
-                    break;
-                case 3:
-                    str = 'Wednesday';
-                    break;
-                case 4:
-                    str = 'Thursday';
-                    break;
-                case 5:
-                    str = 'Friday';
-                    break;
-                case 6:
-                    str = 'Saturday';
-                    break;
-                default:
-                    str = 'Sunday';
-            }
-            self.emit('weekday', weekday, str);
-            self.emit('weekday-' + str);
-            self.emit('weekday-' + str.substr(0, 3));
-            self.emit('weekday-' + str.substr(0, 2));
-            self.emit('weekday-' + weekday);
-            self.weekday = weekday;
+        if (weekday !== last.weekday) {
+            emitter.emit('weekday', weekday, days[weekday - 1])
+            emitter.emit('weekday-' + days[weekday - 1])
+            emitter.emit('weekday-' + days[weekday - 1].substr(0, 3))
+            emitter.emit('weekday-' + weekday)
+            last.weekday = weekday
         }
 
-        if (hour !== self.hour) {
-            self.emit('hour', hour);
-            self.emit('hour-' + hour);
-            if (hour === 0) {
-                self.emit('hour-24');
-            }
-            self.hour = hour;
+        if (hour !== last.hour) {
+            emitter.emit('hour', hour)
+            emitter.emit('hour-' + hour)
+            last.hour = hour
         }
-    };
 
-    var timer = setInterval(eventCollection, interval);
-
-    this.stop = function() {
-        if (timer) {
-            clearInterval(timer);
-            timer = null;
-            self.emit('stop');
+        if (minute !== last.minute) {
+            emitter.emit('minute', minute)
+            emitter.emit('minute-' + minute)
+            last.minute = minute
         }
-    };
-
-    this.start = function() {
-        if (!timer) {
-            timer = setInterval(eventCollection, interval);
-            self.emit('start');
-        }
-    };
-};
-
-util.inherits(DateEmitter, EventEmitter);
+    }
+}
