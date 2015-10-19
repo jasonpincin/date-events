e1a7061ca9a6bebd3bbdbb272d7d2bf2b125a3c5
Jason Pincin <jason@pincin.com>
2015-10-19 18:16:41 -0400

    converted to tape

--- test/exit-on-hour.js
+++ test/exit-on-hour.js
@@ -1,19 +1,12 @@
-var tap         = require("tap"),
-    test        = tap.test,
-    DateEvents;
+var test       = require('tape'),
+    DateEvents = require('..')
 
-test('load module', function(t) {
-     DateEvents = require('../index');
-     t.ok(DateEvents, 'module loaded');
-     t.end();
-});
-
-test('hour event', function(t) {
-    t.plan(1);
-    var cal = new DateEvents();
-    cal.once('hour', function() {
-        t.ok(true, 'got hour event');
-        t.end();
-        cal.removeAllListeners();
-    });
-});
+test('hour event', function (t) {
+    t.plan(1)
+    var cal = new DateEvents()
+    cal.once('hour', function () {
+        t.ok(true, 'got hour event')
+        t.end()
+        cal.removeAllListeners()
+    })
+})
